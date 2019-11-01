71f0a40f216c57ba763ab95db89474a31ab4ac45
Jesse Howe <jchowe93@gmail.com>
2019-11-01 18:44:00 -0500

    add render function

--- library.js
+++ library.js
@@ -1,15 +1,16 @@
 let myLibrary = [];
 
 function Book(title,author,pages,read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`
-    }
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+		return `${title} by ${author}, ${pages} pages, ${read}`
+  }
 }
 
+//!PLACEHOLDER
 const theHobbit = new Book('The Hobbit','J.R.R. Tolkein',295,'not read yet');
 const harryPotter = new Book('Harry Potter: and the Sorcerer\'s stone','J.K. Rowling', 312, 'read');
 const naruto = new Book('Naruto', 'Masashi Kishimoto', 7805, 'read');
@@ -18,15 +19,22 @@ const ABriefHistoryOfSpaceAndTime = new Book('A Brief History of Space and Time'
 
 const addBookToLibrary = book => myLibrary.push(book);
 
+//!PLACEHOLDER
 addBookToLibrary(theHobbit);
 addBookToLibrary(harryPotter);
 addBookToLibrary(naruto);
 addBookToLibrary(toKillAMockingBird);
 addBookToLibrary(ABriefHistoryOfSpaceAndTime);
 
+const bookDisplay = document.getElementById('book-display');
 
 function render() {
-    for(let book in myLibrary){
-        
-    }
+	myLibrary.forEach(function(book) {
+		let bookCard = document.createElement("div");
+		let bookContent = document.createTextNode(`${book.info()}`);
+		bookCard.appendChild(bookContent);  
+		bookDisplay.appendChild(bookCard);
+	});
 }
+
+render();
