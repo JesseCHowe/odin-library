0ee443fcd65b392439d3e4f37343dbe199b49915
Jesse Howe <jchowe93@gmail.com>
2019-11-01 18:21:11 -0500

    add inital library functions

---
+++ library.js
@@ -0,0 +1,32 @@
+let myLibrary = [];
+
+function Book(title,author,pages,read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `${title} by ${author}, ${pages} pages, ${read}`
+    }
+}
+
+const theHobbit = new Book('The Hobbit','J.R.R. Tolkein',295,'not read yet');
+const harryPotter = new Book('Harry Potter: and the Sorcerer\'s stone','J.K. Rowling', 312, 'read');
+const naruto = new Book('Naruto', 'Masashi Kishimoto', 7805, 'read');
+const toKillAMockingBird = new Book('To Kill A Mockingbird', 'Harper Lee', 195, 'not read yet');
+const ABriefHistoryOfSpaceAndTime = new Book('A Brief History of Space and Time', 'Stephen Hawking', 340, 'not read yet');
+
+const addBookToLibrary = book => myLibrary.push(book);
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(harryPotter);
+addBookToLibrary(naruto);
+addBookToLibrary(toKillAMockingBird);
+addBookToLibrary(ABriefHistoryOfSpaceAndTime);
+
+
+function render() {
+    for(let book in myLibrary){
+        
+    }
+}
